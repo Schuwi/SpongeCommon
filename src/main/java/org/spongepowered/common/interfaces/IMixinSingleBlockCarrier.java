23d8db6898c1640c25f6d74b66d475b5834c4fe0
Anselm Brehme <development.anselm@breh.me>
2017-12-08 21:07:34 +0100

    move location methods to multiblock carrier

--- src/main/java/org/spongepowered/common/interfaces/IMixinSingleBlockCarrier.java
+++ src/main/java/org/spongepowered/common/interfaces/IMixinSingleBlockCarrier.java
@@ -27,23 +27,13 @@ package org.spongepowered.common.interfaces;
 import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.api.item.inventory.SingleBlockCarrier;
 import org.spongepowered.api.util.Direction;
-import org.spongepowered.api.world.Location;
-import org.spongepowered.api.world.World;
 
 import java.util.Optional;
 
 public interface IMixinSingleBlockCarrier extends SingleBlockCarrier {
 
     @Override
-    default Optional<Inventory> getInventory(Location<World> at) {
-        if (this.getLocation().equals(at)) {
-            return Optional.of(this.getInventory());
-        }
-        return Optional.empty();
-    }
-
-    @Override
-    default Optional<Inventory> getInventory(Location<World> at, Direction from) {
-        return this.getInventory(at);
+    default Optional<Inventory> getInventory(Direction from) {
+        return Optional.ofNullable(this.getInventory());
     }
 }
